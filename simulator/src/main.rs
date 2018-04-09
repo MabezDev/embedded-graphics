7e32c53dfbc3163b8cfc594d1b1afaa177888045
James Waples <james@cambridge-intelligence.com>
2018-04-09 15:54:19 +0100

    Draw a super slow 128x64 pixel display
    I need to optimise this rather a lot

--- simulator/src/main.rs
+++ simulator/src/main.rs
@@ -1,25 +1,35 @@
 extern crate kiss3d;
 extern crate nalgebra as na;
+extern crate rand;
 
 use kiss3d::light::Light;
 use kiss3d::scene::SceneNode;
 use kiss3d::window::Window;
 use na::{Translation, UnitQuaternion, Vector3};
+use rand::random;
+
+const DISP_WIDTH: u8 = 128;
+const DISP_HEIGHT: u8 = 64;
+const PIXEL_SIZE: f32 = 0.1;
 
 fn main() {
-    let mut window = Window::new("Kiss3d: cube");
+    let mut window = Window::new("Display sim");
 
-    let mut cubes: Vec<Vec<SceneNode>> = (0..10)
+    let left = -(PIXEL_SIZE * DISP_WIDTH as f32 / 2.0);
+    let top = -(PIXEL_SIZE * DISP_HEIGHT as f32 / 2.0);
+
+    let mut cubes: Vec<Vec<SceneNode>> = (0..DISP_HEIGHT)
         .map(|y| {
-            (0..10)
+            (0..DISP_WIDTH)
                 .map(|x| {
-                    let mut c = window.add_cube(0.1, 0.1, 0.1);
+                    let mut c = window.add_quad(0.1, 0.1, 1, 1);
 
                     c.set_local_translation(Translation::from_vector(Vector3::new(
-                        -x as f32 * 0.2,
-                        -y as f32 * 0.2,
+                        left + x as f32 * PIXEL_SIZE,
+                        top + y as f32 * PIXEL_SIZE,
                         0.0,
                     )));
+                    c.set_color(random(), random(), random());
 
                     c
                 })
